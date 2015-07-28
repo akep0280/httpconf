373fb6431ec88c309aa70dcb610a848918716151
akep0280 <akepley@gmail.com>
2015-07-28 14:38:42 -0500

    test

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,13 @@
 class httpconf::config {
 
+  class {'apache'}
+
   file {'/etc/httpd/conf.d/nasba_cis.conf':
     ensure   => 'file',
     owner    => 'root',
     group    => 'root',
     mode     => '0644',
     source   => 'puppet:///modules/httpconf/files/nasba_cis.conf',
-    require  => Package['apache'],
     }
 
   }
