fbfb0fb26798c45feb72eddf02c730456bab731d
akep0280 <akepley@gmail.com>
2015-07-29 11:30:45 -0500

    New config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,7 @@ class httpconf::config {
   apache::custom_config { 'nasba_cis.conf':
     ensure => present,
     source => 'puppet:///modules/httpconf/nasba_cis.conf',
+    require => Package['httpd'],
   }
 
   }
