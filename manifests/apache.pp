84242f9e7f034e40ec97054b63741cecf0e30c38
akep0280 <akepley@gmail.com>
2015-08-06 09:23:48 -0500

    changes to apache

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,4 +1,19 @@
 class { 'apache':
+  include apache::mod::auth_basic
+  include apache::mod::include
+  include apache::mod::setenvif
+  include apache::mod::mime_magic
+  include apache::mod::mime
+  include apache::mod::expires
+  include apache::mod::deflate
+  include apache::mod::vhost_alias
+  include apache::mod::rewrite
+  include apache::mod::cache
+  include apache::mod::disk_cache
+  include apache::mod::cgi
+  include apache::mod::negotiation
+  include apache::mod::dir
   default_mods        => false,
   default_confd_files => false,
+  mpm_module          => prefork,
   }
