87db3240e2b70d7d26f78516c827beae0f7648bd
akep0280 <akepley@gmail.com>
2015-08-06 10:57:44 -0500

    apache change

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,5 @@
 class httpconf::config {
-
+  include stdlib
 
   file {'/etc/httpd/conf.d/nasba_cis.conf':
     ensure   => 'file',
@@ -7,6 +7,7 @@ class httpconf::config {
     group    => 'root',
     mode     => '0644',
     source   => 'puppet:///modules/httpconf/nasba_cis.conf',
+    notify   => Service["httpd"],
     require  => Package['httpd'],
     }
 
@@ -18,4 +19,30 @@ class httpconf::config {
       source   => 'puppet:///modules/httpconf/geoip.conf',
       require  => Package['httpd'],
       }
+
+  package { 'mod_geoip':
+    ensure => 'present',
+    }
+
+  file { '/etc/httpd/vhosts.d':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['httpd'],
   }
+
+  file_line { 'Indexes_disable_15-default':
+    path  => '/etc/httpd/conf.d/15-default.conf',
+    line  => '    Options -Indexes FollowSymLinks MultiViews',
+    match => '^    Options Indexes FollowSymLinks MultiViews',
+    notify => Service["httpd"],
+  }
+
+  file_line { 'Indexes_disable_alias':
+    path  => '/etc/httpd/conf.d/alias.conf',
+    line  => '    Options -Indexes MultiViews',
+    match => '^    Options Indexes MultiViews',
+    notify => Service["httpd"],
+  }
+}
